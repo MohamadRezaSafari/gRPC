926cd1566c3c452cf360a5d16a329848ac0822a3
Mohamad Reza Safari <mrdev1020@gmail.com>
2021-07-06 19:51:00 +0430

    Add Repository

---
+++ MeterReader/Server/MeterReaderWeb/Services/MeterService.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Core;
+using MeterReaderWeb.Data;
+using MeterReaderWeb.Data.Entities;
+using MeterReaderWeb.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeterReaderWeb.Services
+{
+    public class MeterService : MeterReadingService.MeterReadingServiceBase
+    {
+        private readonly ILogger<MeterService> logger;
+        private readonly IReadingRepository repository;
+
+        public MeterService(ILogger<MeterService> logger, IReadingRepository repository)
+        {
+            this.logger = logger;
+            this.repository = repository;
+        }
+
+
+        public override async Task<StatusMessage> AddReading(ReadingPacket request, ServerCallContext context)
+        {
+            var result = new StatusMessage()
+            {
+                Success = ReadingStatus.Failure
+            };
+
+            if (request.Successful == ReadingStatus.Success)
+            {
+                try
+                {
+                    foreach (var r in request.Readings)
+                    {
+                        var reading = new MeterReading()
+                        {
+                            Value = r.ReadingValue,
+                            ReadingDate = r.ReadingTime.ToDateTime(),
+                            CustomerId = r.CustomerId
+                        };
+
+                        repository.AddEntity(reading);
+                    }
+
+                    if (await repository.SaveAllAsync())
+                    {
+                        result.Success = ReadingStatus.Success;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    result.Message = "Exception thrown during proccess";
+                    logger.LogError($"Exception thrown during saving of reading: {ex}");
+                }
+            }
+
+            return result;
+        }
+    }
+}
