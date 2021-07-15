22fd4c5cfbcc679975d8dbf98908051653d473b5
Mohamad Reza Safari <mrdev1020@gmail.com>
2021-07-15 21:23:09 +0430

    Complete

---
+++ GrpcService/GrpcClientApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            var customerClient = new Customer.CustomerClient(channel);
+            var result = await customerClient.GetCustomerInfoAsync(new CustomerFindModel()
+            {
+                UserId = 1
+            });                       
+
+            Console.WriteLine($"First Name: {result.FirstName} \nLastName: {result.LastName}");
+
+
+            var allCustomers = customerClient.GetAllCustomers(new AllCustomerModel());
+            var allCustomersList = allCustomers.ResponseStream.ReadAllAsync().GetAsyncEnumerator();
+
+            await foreach (var item in allCustomers.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- GrpcService/GrpcService/Services/CustomerService.cs
+++ GrpcService/GrpcService/Services/CustomerService.cs
@@ -16,11 +16,43 @@ namespace GrpcService.Services
             this.logger = logger;
         }
 
+        public override async Task GetAllCustomers(AllCustomerModel request, IServerStreamWriter<CustomerDataModel> responseStream, ServerCallContext context)
+        {
+            var allCustomers = new List<CustomerDataModel>();
+
+            var c1 = new CustomerDataModel();
+            c1.FirstName = "Bruce";
+            c1.LastName = "Wayne";
+            allCustomers.Add(c1);
+
+            var c2 = new CustomerDataModel();
+            c2.FirstName = "Homer";
+            c2.LastName = "Simpsons";
+            allCustomers.Add(c2);
+
+            var c3 = new CustomerDataModel();
+            c3.FirstName = "Tony";
+            c3.LastName = "Stark";
+            allCustomers.Add(c3);
+
+            var c4 = new CustomerDataModel();
+            c4.FirstName = "Mohamad-Reza";
+            c4.LastName = "Safari";
+            allCustomers.Add(c4);
+
+
+            foreach (var item in allCustomers)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+
         public override Task<CustomerDataModel> GetCustomerInfo(CustomerFindModel request, ServerCallContext context)
         {
             var model = new CustomerDataModel();
 
-            if(request.UserId == 1)
+            if (request.UserId == 1)
             {
                 model.FirstName = "Mohamad Reza";
                 model.LastName = "Safari";
@@ -30,6 +62,8 @@ namespace GrpcService.Services
                 model.FirstName = "...";
                 model.LastName = "...";
             }
+
+            return Task.FromResult(model);
         }
     }
 }
